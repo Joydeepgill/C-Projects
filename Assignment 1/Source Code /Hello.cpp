afecded303df3cf6ca4940000b6064bca4351ae6
Joydeep Gill <joyydeep.gill@gmail.com>
2023-10-27 00:51:08 -0400

    First commit, adding all the C++ files

---
+++ Assignment 1/Source Code /Hello.cpp
@@ -0,0 +1,11 @@
+//2.1 - Hello World I  
+
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+    const string greeting = "Hello NYC!"; 
+    cout << greeting << endl; 
+    return 0; 
+}
