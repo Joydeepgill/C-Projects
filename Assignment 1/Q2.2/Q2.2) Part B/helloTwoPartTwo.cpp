adba7566a821121e373970982c158b9ee7f7f7c5
Joydeep Gill <joyydeep.gill@gmail.com>
2023-11-21 22:28:39 -0500

    Adding Q)2.2 Part B

---
+++ Assignment 1/Q2.2/Q2.2) Part B/helloTwoPartTwo.cpp
@@ -0,0 +1,21 @@
+//Q 2.2) Part B - While loop 
+
+#include <iostream> 
+
+using namespace std;  
+
+int main() {
+
+    int num; 
+
+    cin >> num;  
+
+    int i = 0; 
+
+    while (i < num) {
+        cout << "Hello NYC" << endl;  
+        i++; 
+    }
+
+    return 0; 
+}
