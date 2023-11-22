6611d8abfde1ba3e1f4e1fc3ebe74e2b7992bf4b
Joydeep Gill <joyydeep.gill@gmail.com>
2023-11-21 22:19:57 -0500

    Added Q2.2

---
+++ Assignment 1/Q2.2/helloTwo.cpp
@@ -0,0 +1,15 @@
+//2.2 (a) - Using a For Loop 
+
+#include <iostream> 
+
+using namespace std; 
+
+int main() { 
+    int num; 
+    cin >> num; 
+
+    for(int i = 0; i < num; i++) {
+        cout << "Hello NYC" << endl; 
+    }
+    return 0; 
+}
