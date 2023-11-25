cbded36b91bc0a54bd2d007bb448e00c4674d72e
Joydeep Gill <joyydeep.gill@gmail.com>
2023-11-24 19:22:42 -0500

    added Q2.2 Part B

---
+++ Assignment 1/Q2.2/Q2.2) Part C/helloPart2.3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {   
+
+    int maxInput; 
+
+    cin >> maxInput; 
+
+    int i = 0; 
+
+    do {
+        cout << "Hello World" << endl; 
+        i++; 
+    }
+
+    while (i < maxInput); 
+
+    return 0; 
+}
